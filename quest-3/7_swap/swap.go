2d394115107677165efb159d656c276c5779be77
el mehhdi <lmehdi0006@gmail.com>
2024-12-01 18:41:44 +0100

    pool prep

---
+++ quest-3/7_swap/swap.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func swap(a *int, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
+
+}
+
+func main() {
+	a := 20
+	b := 1
+	swap(&a, &b)
+	fmt.Println(a)
+	fmt.Println(b)
+
+}
